ff491804a4c0f1cc8cdb97f339bbe28cfa2cd933
Fabian Thiellier (2source4) <ffamaster.z@gmail.com>
2017-10-05 15:21:30 +0200

    Initial commit

---
+++ hi.js
@@ -0,0 +1,7 @@
+$(() => {
+    let message =
+`Hey there!
+You opened the console here, good job, go try some stuff out!`
+    console.log(message)
+    // Did you expect to find anything more here?
+});
